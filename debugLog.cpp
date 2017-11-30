20bb45e674f32af0c5051d92d07d3349cc9b2518
oxydated <oxydation@gmail.com>
2017-11-29 22:45:38 -0200

    New debug functions for double, complex and 3x3 matrices

--- debugLog.cpp
+++ debugLog.cpp
@@ -15,6 +15,7 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = {";
 			outStream << dualQuat[0] << "," << dualQuat[1] << "," << dualQuat[2] << "," << dualQuat[3] << ",";
 			outStream << dualQuat[4] << "," << dualQuat[5] << "," << dualQuat[6] << "," << dualQuat[7];
@@ -29,6 +30,7 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = {";
 			outStream << qs << "," << qx << "," << qy << "," << qz << ",";
 			outStream << dqs << "," << dqx << "," << dqy << "," << dqz;
@@ -46,6 +48,7 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = N[makeDualVersor[" << angle << ", {" << nx << "," << ny << "," << nz << " }, " << slide << ", {" << mx << "," << my << "," << mz << " }]]" << std::endl;
 			outStream << std::endl;
 
@@ -57,6 +60,23 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
+			outStream << name << " = Transpose[{";
+			outStream << "{" << m[0] << ", " << m[1] << ", " << m[2] << ", " << m[3] << "}" << ",";
+			outStream << "{" << m[4] << ", " << m[5] << ", " << m[6] << ", " << m[7] << "}" << ",";
+			outStream << "{" << m[8] << ", " << m[9] << ", " << m[10] << ", " << m[11] << "}" << ",";
+			outStream << "{" << m[12] << ", " << m[13] << ", " << m[14] << ", " << m[15] << "}" << "}]";
+			outStream << std::endl;
+
+			resultString = outStream.str();
+			OutputDebugString(resultString.c_str());
+		}
+
+		void printMatrix(std::wstring name, const double* m)
+		{
+			outStream.str(outStr);
+			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = Transpose[{";
 			outStream << "{" << m[0] << ", " << m[1] << ", " << m[2] << ", " << m[3] << "}" << ",";
 			outStream << "{" << m[4] << ", " << m[5] << ", " << m[6] << ", " << m[7] << "}" << ",";
@@ -72,6 +92,7 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = {";
 			outStream << px << "," << py << "," << pz;
 			outStream << "}";
@@ -85,6 +106,7 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = "<< wstr << std::endl;
 			resultString = outStream.str();
 			OutputDebugString(resultString.c_str());
@@ -94,9 +116,67 @@ namespace oxyde {
 		{
 			outStream.str(outStr);
 			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
 			outStream << name << " = " << par << std::endl;
 			resultString = outStream.str();
 			OutputDebugString(resultString.c_str());
+		}
+
+		void printNamedParameter(std::wstring name, int par)
+		{
+			outStream.str(outStr);
+			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
+			outStream << name << " = " << par << std::endl;
+			resultString = outStream.str();
+			OutputDebugString(resultString.c_str());
+		}
+
+		void printNamedParameter(std::wstring name, complex par)
+		{
+			outStream.str(outStr);
+			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
+			outStream << name << " = " << complexToWstr(par) << std::endl;
+			resultString = outStream.str();
+			OutputDebugString(resultString.c_str());
+		}
+
+		void printComplexVector(std::wstring name, const complex px, const complex py, const complex pz)
+		{
+			outStream.str(outStr);
+			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
+			outStream << name << " = {";
+			outStream << complexToWstr(px) << "," << complexToWstr(py) << "," << complexToWstr(pz);
+			outStream << "}";
+			outStream << std::endl;
+
+			resultString = outStream.str();
+			OutputDebugString(resultString.c_str());
+		}
+
+		void print3x3ComplexMatrix(std::wstring name, const complex m[]) {
+			outStream.str(outStr);
+			outStream << std::fixed;
+			outStream << "\"" << name << "\"" << std::endl;
+			outStream << name << " = Transpose[{";
+			outStream << "{" << complexToWstr(m[0]) << ", " << complexToWstr(m[1]) << ", " << complexToWstr(m[2]) << "}" << ",";
+			outStream << "{" << complexToWstr(m[3]) << ", " << complexToWstr(m[4]) << ", " << complexToWstr(m[5]) << "}" << ",";
+			outStream << "{" << complexToWstr(m[6]) << ", " << complexToWstr(m[7]) << ", " << complexToWstr(m[8]) << "}" << "}]";
+			outStream << std::endl;
+
+			resultString = outStream.str();
+			OutputDebugString(resultString.c_str());
+		}
+
+		std::wstring complexToWstr(complex num)
+		{
+			std::wstring convStr;
+			std::wostringstream convStream(convStr);
+			convStream << std::fixed;
+			convStream << std::real(num) << L" + " << std::imag(num) << L" I ";
+			return  convStream.str();
 		}
 
 		void printLine() {
--- debugLog.h
+++ debugLog.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <string>
+#include <complex>
 
 namespace oxyde {
 	namespace log {
+
+		using complex = std::complex<double>;
+
 		void printDualQuat(std::wstring name, const float* dualQuat);
 
 		void printDualQuat(std::wstring name, const float qs, const float qx, const float qy, const float qz, const float dqs, const float dqx, const float dqy, const float dqz);
@@ -13,6 +17,8 @@ namespace oxyde {
 			const float mx, const float my, const float mz);
 
 		void printMatrix(std::wstring name, const float* m);
+
+		void printMatrix(std::wstring name, const double* m);
 
 		void printPointInSpace(std::wstring name, const float px, const float py, const float pz);
 
@@ -25,5 +31,15 @@ namespace oxyde {
 		void printNamedParameter(std::wstring name, std::wstring wstr);
 
 		void printNamedParameter(std::wstring name, float par);
+
+		void printNamedParameter(std::wstring name, int par);
+
+		void printNamedParameter(std::wstring name, complex par);
+
+		void printComplexVector(std::wstring name, const complex px, const complex py, const complex pz);
+
+		void print3x3ComplexMatrix(std::wstring name, const complex m[]);
+
+		std::wstring complexToWstr(complex num);
 	}
 }
